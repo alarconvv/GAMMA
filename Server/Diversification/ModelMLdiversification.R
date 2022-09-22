65e8b088bb8b5c39dbf9a1d45d8f73de234c5ab3
Guane <alarconvv@gmail.com>
2022-09-22 16:17:57 -0300

    220921.1

--- Server/Diversification/ModelMLdiversification.R
+++ Server/Diversification/ModelMLdiversification.R
@@ -9,8 +9,11 @@ DiverModML$iterObjectDiver <- NULL
 DiverModML$runObjModels <- NULL
 DiverModML$YuleList <-list()
 DiverModML$iteryule<-list()
+DiverModML$BDList <-list()
+DiverModML$iterBD<-list()
 
 DiverModML$countyule <- 0
+DiverModML$countBD <- 0
 
 #Render print in Info panel: Models: ML
 #
@@ -47,12 +50,10 @@ output$PhyloPlotDiver5 <- renderPlot( height = heightDiverModML  , width = width
 
 
 
-#models
+# Yule Model
 
 observeEvent(input$yuleModML,{
   DiverModML$runObjModels$Yule <- NULL
-  
-  
 })
 
 observeEvent(input$addYuleModML,{
@@ -66,23 +67,85 @@ observeEvent(input$addYuleModML,{
     names(DiverModML$YuleList)[DiverModML$countyule] <- paste('Yule',DiverModML$countyule, sep = '')
     
     DiverModML$runObjModels$Yule <- DiverModML$YuleList
-    
-  
-  
-  
-  
-   
-
 })
 
-
 #Temporal object to print in info panel
-# info: tree
+# info:  models
 observeEvent(input$addYuleModML, {
   DiverModML$iterObjectDiver <- DiverModML$runObjModels
 })
 
 
+
+# BD Model
+
+observeEvent(input$BDContModML,{
+  DiverModML$runObjModels$BD <- NULL
+  
+  ratesBDTable <- matrix(data = c(0.1,0.1),nrow = 1,ncol = 2)
+  colnames(ratesBDTable) <- c('Sp', 'Ext')
+  row.names(ratesBDTable) <- 'Rates'
+  
+  output$BDrateModML <- renderRHandsontable({rhandsontable(ratesBDTable, readOnly = F)})
+})
+
+observeEvent(input$addBDContModML,{
+  
+  DiverModML$iterBD$rho <- as.numeric(input$fractBDContModML)
+  DiverModML$iterBD$Brate <- as.numeric(hot_to_r(input$BDrateModML)[1,1])
+  DiverModML$iterBD$Drate <- as.numeric(hot_to_r(input$BDrateModML)[1,2])
+  DiverModML$BDList$model <- DiverModML$iterBD
+  
+  DiverModML$countBD <- DiverModML$countBD + 1
+  
+  names(DiverModML$BDList)[DiverModML$countBD] <- paste('BD',DiverModML$countBD, sep = '')
+  
+  DiverModML$runObjModels$BD <- DiverModML$BDList
+})
+
+
+#Temporal object to print in info panel
+# info:  models
+observeEvent(input$addBDContModML, {
+  DiverModML$iterObjectDiver <- DiverModML$runObjModels
+})
+
+
+
+# BDvarSp Model
+
+observeEvent(input$BDContModML,{
+  DiverModML$runObjModels$BD <- NULL
+  
+  
+  
+  ratesBDTable <- matrix(data = c(0.1,0.1),nrow = 1,ncol = 2)
+  colnames(ratesBDTable) <- c('Sp', 'Ext')
+  row.names(ratesBDTable) <- 'Rates'
+  
+  output$BDrateModML <- renderRHandsontable({rhandsontable(ratesBDTable, readOnly = F)})
+})
+
+observeEvent(input$addBDContModML,{
+  
+  DiverModML$iterBD$rho <- as.numeric(input$fractBDContModML)
+  DiverModML$iterBD$Brate <- as.numeric(hot_to_r(input$BDrateModML)[1,1])
+  DiverModML$iterBD$Drate <- as.numeric(hot_to_r(input$BDrateModML)[1,2])
+  DiverModML$BDList$model <- DiverModML$iterBD
+  
+  DiverModML$countBD <- DiverModML$countBD + 1
+  
+  names(DiverModML$BDList)[DiverModML$countBD] <- paste('BD',DiverModML$countBD, sep = '')
+  
+  DiverModML$runObjModels$BD <- DiverModML$BDList
+})
+
+
+#Temporal object to print in info panel
+# info:  models
+observeEvent(input$addBDContModML, {
+  DiverModML$iterObjectDiver <- DiverModML$runObjModels
+})
 
 ### models
 observeEvent(input$ModelRunModML,{
@@ -90,7 +153,6 @@ observeEvent(input$ModelRunModML,{
  
   if (input$yuleModML){
     #unresolve
-    
     if (input$optModML == 'optim'){
       yule <-make.yule(tree = treeModML(), sampling.f = as.numeric(input$fractYuleModML), unresolved = NULL)
       fityule <- find.mle(func = yule, x.init= as.numeric(input$BrateYuleModML), method='optim', control = list(optim.method= input$optimModML))
